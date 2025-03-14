814279cb998571617bb5ae4efedf1751ffc90859
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2025-03-13 21:30:48 -0600

    chore(functions): add phone recharge excercise

--- fundamentals/functions/phone-recharge.js
+++ fundamentals/functions/phone-recharge.js
@@ -3,33 +3,42 @@ const prompt = require('prompt-sync')()
 const MIN_RECHARGE = 20
 const MAX_RECHARGE = 80
 const MAX_RECHARGE_SUPERSAVER = 38
-const MIN_YEAR_SUBSCRIPTION = 1
-const YEAR_SUBSCRIPTION_PROMOS = 3
+const MIN_MONTH_SUBSCRIPTION = 12
+const SUPERSAVER_PROMO_MAX_DURATION = 36
+const SUPERSAVER_PROMO_MULTIPPLIER = 2
+const MEGA_PROMO_MIN_DURATION = 36
+const MEGA_PROMO_MULTIPLIER = 3
 
-function validatePromo(name, recharge, timeSubscription){
-const typePromo =''
-const promoRecharge = 0
+function calculateRecharge(amount, timeSubscription){
 
-    if (recharge < MIN_RECHARGE && recharge > MAX_RECHARGE && timeSubscription >=1) return false
+    if (timeSubscription < MIN_MONTH_SUBSCRIPTION || amount < MIN_RECHARGE || amount > MAX_RECHARGE) return amount
 
-    if (recharge > 38 && timeSubscription <= YEAR_SUBSCRIPTION_PROMOS) {
-            typePromo = "SuperSaver"
-            promoRecharge = 2
-        } else if (timeSubscription > YEAR_SUBSCRIPTION_PROMOS){
-            typePromo = "MegaRechargable"
-            promoRecharge = 3
-        }
+    let promoMultiplier = 1
+
+    if ( amount < MAX_RECHARGE_SUPERSAVER && timeSubscription <= SUPERSAVER_PROMO_MAX_DURATION){
+        promoMultiplier = SUPERSAVER_PROMO_MULTIPPLIER
+    } else if (timeSubscription > MEGA_PROMO_MIN_DURATION){
+        promoMultiplier = MEGA_PROMO_MULTIPLIER
+    }
+    return amount * promoMultiplier          
+        
     }
 
 function main(){
     
     const name = prompt("Name: ")
-    const recharge = parseFloat(prompt('Recharge Amount(USD): '))
-    const timeSubscription = parseFloat(prompt("Subscription Time: "))
+    const amount = parseInt(prompt('Recharge Amount(USD): '))
+    const timeSubscription = parseInt(prompt("Subscription Time (months): "))
 
+    const recharge = calculateRecharge(amount,timeSubscription)
+    const promo = recharge - amount
 
+    console.log(
+        `THANKS ${name}
+        RECHARGE: ${recharge}
+        PROMO: ${promo}`
+    )
 
-    console.log(name,recharge)
 }
 
 main()
