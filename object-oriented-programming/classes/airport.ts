38a081be6c82ef1e640144f22830523e52e0c758
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2025-03-20 22:45:08 -0600

    chore(classes): add airport excercise

---
+++ object-oriented-programming/classes/airport.ts
@@ -0,0 +1,53 @@
+type TrafficLevel = 'low' | 'middle' | 'high'
+
+class Airport{
+    private _name:string
+    private _location:string
+    private _runwayStatus: boolean
+    private _trafficLevel: TrafficLevel
+   
+    constructor(name : string, location: string
+    ){
+        this._name = name
+        this._location = location
+        this._runwayStatus = true
+        this._trafficLevel = 'low'
+    }
+
+    get runwayStatus(){
+        return this._runwayStatus
+    }
+    set runwayStatus(status:boolean){
+        this._runwayStatus = status
+    }
+    get trafficLevel(){
+        return this._trafficLevel
+    }
+    set trafficLevel(level:TrafficLevel){
+        this._trafficLevel = level
+    }
+    changeTrafficLevel (level:TrafficLevel): void{
+        this.trafficLevel = level
+        console.log(`El nivel de tráfico se ha cambiado a ${level} en el aeropuerto ${this._name}`)
+    }
+
+    emergencyLanding():void{
+        this.runwayStatus = false
+        this.trafficLevel = 'high'
+        console.log(`Aterrizaje de emergencia en el aeropuerto ${this._name}. La pista esta cerrada`)
+    }
+    displayInfo(): void{
+        console.log(`Nombre: ${this._name}`)
+        console.log(`Location: ${this._location}`)
+        console.log(`Estado de la pista: ${this.runwayStatus}`)
+        console.log(`Trafico: ${this.trafficLevel}`)
+    }
+}
+
+const myAirport = new Airport('Aeropuerto Int', 'Ciudad ABC')
+
+myAirport.displayInfo()
+myAirport.changeTrafficLevel('middle')
+myAirport.displayInfo()
+myAirport.emergencyLanding()
+myAirport.displayInfo()
