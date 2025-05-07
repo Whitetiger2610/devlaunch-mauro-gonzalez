ebd3b102abe57442d50de954ee655ee7e493a1fe
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2025-05-06 22:27:18 -0500

    chore(loops): add tarot numbers exercise

---
+++ data-structures/loops/dog-race.ts
@@ -0,0 +1,17 @@
+// dog race 🐾
+
+const MAX_STEP = 3
+const MIN_STEP = 1
+const LINE_LENGTH = 20
+
+console.log("Pista Inicial")
+console.log("🏁|--------------------🐩")
+console.log("🏁|--------------------🦮")
+
+export const getRaceResult = () =>{
+  let dog1: string = "🏁|--------------------🐩"
+  let dog2: string = "🏁|--------------------🐩"
+}
+getRaceResult()
+
+
