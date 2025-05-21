e1cd147ed705d136ede8a74d024a8136fd866a6f
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2025-05-20 21:46:30 -0500

    chore(array): add market exercise

--- data-structures/array/market.ts
+++ data-structures/array/market.ts
@@ -1,1 +1,27 @@
 //market exercise
+
+const calculateDiscount = (prices: number[], n:number, baseDiscount: number = 0.2) => {
+
+  let subtotal = 0
+  let discount = 0
+  let batchCount = 0
+
+  prices.sort().forEach(price =>{
+    batchCount++
+    subtotal += price
+
+    const discountTime = batchCount === n
+
+    if (discountTime){
+      discount += subtotal*baseDiscount
+      baseDiscount /= 2
+      batchCount = 0
+    }
+  })
+
+  const total = subtotal - discount
+
+  return {total,subtotal,discount}
+}
+
+console.log(calculateDiscount([3000,1000,2000],2))
