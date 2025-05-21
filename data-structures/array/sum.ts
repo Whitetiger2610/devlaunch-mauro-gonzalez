ef83953899d0987511f8b3628509303f73901bdb
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2025-05-20 21:34:04 -0500

    chore(array): add sum exercise

--- data-structures/array/sum.ts
+++ data-structures/array/sum.ts
@@ -1,1 +1,16 @@
 // Sum exercise
+
+const arraySum = (arr1: number[], arr2: number[]): number[] =>{
+  const result: number[] = []
+  const maxLength = Math.max(arr1.length,arr2.length)
+
+  for (let i = 0; i < maxLength; i++){
+    const value1 = arr1[i] || 0
+    const value2 = arr2[i] || 0
+    result.push(value1 + value2)
+  }
+
+  return result
+}
+
+console.log(arraySum([1,0,2,3,4],[3,5,6,7,8,13,9]))
