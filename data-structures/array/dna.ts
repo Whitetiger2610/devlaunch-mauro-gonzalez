dda978aa795e4b0f01ccbe41e86158d31cea4bd6
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2025-05-20 21:27:09 -0500

    chore(array): add dna exercise

--- data-structures/array/dna.ts
+++ data-structures/array/dna.ts
@@ -1,1 +1,17 @@
 // DNA exercise
+
+const dna: string = "gtggggggtttatgcctttagaacagacjgatetag"
+
+const subsequence: string = 'aga'
+
+const isSubsequenceInDNA = (dna:string, match: string) : boolean =>{
+  let subsequence = ""
+
+  for (let i = 0; i <= dna.length - match.length; i++){
+    subsequence = dna.slice(i, i + match.length)
+    if ( subsequence === match) return true
+  }
+  return false
+}
+
+console.log(isSubsequenceInDNA(dna,subsequence))
